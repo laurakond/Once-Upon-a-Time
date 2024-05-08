50b976ffbaabab49d08394e64b1c27286249aa33
laurakond <laura.kondrataite@gmail.com>
2024-05-08 18:50:27 +0100

    py(story.py: edit story layout)

--- story.py
+++ story.py
@@ -3,7 +3,7 @@ intro = ["Once upon a time in a far away land, ",
          "After defeating the Evil Queen,",
          "peace was brought to their Kingdom. ",
          "Snow White and Prince Charming were happily expecting ",
-         "an addition to their family - a little baby called {Emma}. ",
+         "an addition to their family - a little baby called {Emma}.\n",
          "Alas, their joy was shortlived. ",
          "The Evil Queen announced she was brewing a curse. ",
          "The curse that would trap the whole kingdom ",
@@ -11,11 +11,11 @@ intro = ["Once upon a time in a far away land, ",
          "A city called StoryBrooke, Maine, USA… ",
          "where all fairytale characters would ",
          "be trapped in unhappy stories, ",
-         "clueless of who they really are. ",
+         "clueless of who they really are.\n",
          "Worried about the hapless future, ",
-         "Snow and Charming set out to stop the Evil Queen. "]
+         "Snow and Charming set out to stop the Evil Queen.\n"]
 
-chapter1 = ["Chapter 1",
+chapter1 = ["Chapter 1\n",
             "They searched for solutions and ways to stop her.",
             "Rumours reached them that Rumpelstiltskin",
             "would be able to help, ",
@@ -31,11 +31,11 @@ chapter1_cont = ["Snow decides to speak to Rumpel.",
                  "However, there was a condition attached. ",
                  "Rumpel asked for the baby’s name:\n"]
 
-chapter2 = ["Chapter 1",
+chapter2 = ["Chapter 2\n",
             "On the day of the planned escape, a purple curse cloud arose…",
             "The Queen’s Army has stormed the Castle, ",
             "forcing Snow and Prince to rush to the wardrobe. ",
-            "Alas their baby decided to come. ",
+            "Alas their baby decided to come.\n",
             "The army is at the end of the corridor. ",
             "Should Snow and Charming:\n"]
 
