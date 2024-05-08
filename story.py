bc8994957447ea7677449b87773509ba97383834
laurakond <laura.kondrataite@gmail.com>
2024-05-08 18:32:23 +0100

    py(add storyline lists to story.py file)

---
+++ story.py
@@ -0,0 +1,59 @@
+intro = ["Once upon a time in a far away land, ",
+         "there lived Snow White with her prince charming. ",
+         "After defeating the Evil Queen,",
+         "peace was brought to their Kingdom. ",
+         "Snow White and Prince Charming were happily expecting ",
+         "an addition to their family - a little baby called {Emma}. ",
+         "Alas, their joy was shortlived. ",
+         "The Evil Queen announced she was brewing a curse. ",
+         "The curse that would trap the whole kingdom ",
+         "in her fantasy world in a far away land. ",
+         "A city called StoryBrooke, Maine, USA… ",
+         "where all fairytale characters would ",
+         "be trapped in unhappy stories, ",
+         "clueless of who they really are. ",
+         "Worried about the hapless future, ",
+         "Snow and Charming set out to stop the Evil Queen. "]
+
+chapter1 = ["Chapter 1",
+            "They searched for solutions and ways to stop her.",
+            "Rumours reached them that Rumpelstiltskin",
+            "would be able to help, ",
+            "however, Snow was aware of his 'deals'",
+            "and was cautious to approach him. ",
+            "Should Snow and Charming:\n"]
+
+chapter1_cont = ["Snow decides to speak to Rumpel.",
+                 "Upon the visit, Rumpel explains",
+                 "that the only way to save them ",
+                 "all was to hide Snow White in a magical wardrobe ",
+                 "so she could be transported to the unknown land. ",
+                 "However, there was a condition attached. ",
+                 "Rumpel asked for the baby’s name:\n"]
+
+chapter2 = ["Chapter 1",
+            "On the day of the planned escape, a purple curse cloud arose…",
+            "The Queen’s Army has stormed the Castle, ",
+            "forcing Snow and Prince to rush to the wardrobe. ",
+            "Alas their baby decided to come. ",
+            "The army is at the end of the corridor. ",
+            "Should Snow and Charming:\n"]
+
+chapter2_cont = ["They run down the secret stairs a level below",
+                 "where the wardrobe room is located. ",
+                 "Snow and Charming are noticed by a soldier ",
+                 "and starts marching their way:\n"]
+
+chapter2_cont2 = ["The soldier is behind the door trying to break in. ",
+                  "Snow and the baby steps into the wardrobe ",
+                  "but it does not work. ",
+                  "The soldier manages to break through the door ",
+                  "and is marching towards them.\n"]
+
+chapter2_cont3 = ["The curse is released and the fairytale characters ",
+                  "find themselves in StoryBrooke, ",
+                  "where they are stripped of their memories",
+                  "and living hapless lives, ",
+                  "while the Queen revels in her accomplishment.\n",
+                  "But hope is not lost, as {Emma} has managed to escape. ",
+                  "Will the curse be broken?"]
