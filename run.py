d64e30edf94d2b3e904d7953e2ed27ba2024063f
laurakond <laura.kondrataite@gmail.com>
2024-05-08 18:47:47 +0100

    py(run.py: import story.py and check it pulls data)

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import story
+
+for sentence in story.intro:
+    print(sentence)
+
+for sentence in story.chapter1:
+    print(sentence)
+
+for sentence in story.chapter1_cont:
+    print(sentence)
+
+for sentence in story.chapter2:
+    print(sentence)
+
+for sentence in story.chapter2_cont:
+    print(sentence)
+
+for sentence in story.chapter2_cont2:
+    print(sentence)
+
+for sentence in story.chapter2_cont3:
+    print(sentence)
+
+
+#print(story.intro)
+#print(story.chapter1)
+#print(story.chapter1_cont)
+#print(story.chapter2)
+#print(story.chapter2_cont)
+#print(story.chapter2_cont2)
+#print(story.chapter2_cont3)
