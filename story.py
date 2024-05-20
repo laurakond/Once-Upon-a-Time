d3c0e6d054f4d2404d82f31f548570365cb98455
laurakond <laura.kondrataite@gmail.com>
2024-05-20 16:39:00 +0100

    py(story: run code through linter)

--- story.py
+++ story.py
@@ -10,28 +10,27 @@ story_dict = {
                    "Please choose from the following two options. \n",
                    "1. Read instructions",
                    "2. Go to the story\n\n"],
-    "game_instructions": ["\nWhen you start the game, you will need to enter \n",
-                          "your name and gender. This will customise the \n",
-                          "game play. Once inside the story, you will be \n",
-                          "prompted to choose between two options to see \n",
-                          "the outcome of the game. Enter your preferred \n",
-                          "option and see if you win!\n\n"
+    "game_instructions": ["\nAt the start of the game, you will be prompted\n",
+                          "to enter your name and gender. This will\n",
+                          "customise the game play.\n Once the game starts,",
+                          "you will be prompted to choose between two\n",
+                          "options to guess the progression of the story.\n",
+                          "Enter your preferred option and see if you win!\n\n"
                           "Enjoy, and good luck!\n\n"],
-    "story_intro": ["...in a far away land, there lived Snow White with her \n",
-                    "prince charming. After defeating the Evil Queen, peace \n",
-                    "was brought to their Kingdom. Snow White and Prince \n",
-                    "Charming were happily expecting an addition to their\n",
-                    "family - a little baby called {user_name}.\n\n",
-                    "Alas, their joy was shortlived. The Evil Queen \n",
-                    "announced she was brewing a curse. The curse that \n",
-                    "would trap the kingdom in her fantasy world in \n",
-                    "a far away land. A city called StoryBrooke, in Maine, \n",
-                    "USA… where all fairytale characters would be trapped \n",
-                    "in unhappy stories, clueless of who they really are. \n",
-                    "Worried about the hapless future, Snow and Charming \n",
+    "story_intro": ["...in a far away land, there lived Snow White with\n",
+                    "her prince charming. After defeating the Evil Queen,\n",
+                    "peace was brought to their Kingdom. Snow White and\n",
+                    "Prince Charming were happily expecting an addition to\n",
+                    "their family - a little baby called {user_name}.\n\n",
+                    "Alas, their joy was shortlived. The Evil Queen\n",
+                    "announced she was brewing a curse. The curse that\n",
+                    "would trap the kingdom in her fantasy world in\n",
+                    "a far away land. A city called StoryBrooke, in Maine,\n",
+                    "USA… where all fairytale characters would be trapped\n",
+                    "in unhappy stories, clueless of who they really are.\n",
+                    "Worried about the hapless future, Snow and Charming\n",
                     "set out to stop the Evil Queen.\n"],
-    "chapter1": [#"Chapter 1\n\n",
-                 "They searched for solutions and ways to stop her.\n",
+    "chapter1": ["They searched for solutions and ways to stop her.\n",
                  "Rumours reached that Rumpelstiltskin would be able\n",
                  "to help them. Yet, Snow was aware of his 'dealings', \n",
                  "and was cautious to approach him. \n\n",
@@ -58,15 +57,14 @@ story_dict = {
                 "regardless. Unfortunately, they had no other solution.\n\n",
                 "The curse comes and everyone in the Kingdom is banished \n",
                 "for life.\n\nYou were unsuccessful!"],
-    "chapter2": [#"\nChapter 2\n\n",
-                  "On the day of the planned escape, a purple curse cloud \n",
-                  "arose… The Queen’s Army has stormed the Castle, \n",
-                  "forcing Snow and Charming to rush to the wardrobe. \n",
-                  "Alas, their baby decided to come.\n\n",
-                  "The army is at the end of the corridor. \n\n",
-                  "Should Snow and Charming:\n\n",
-                  "1. Fight the Queen's army.\n",
-                  "2. Escape through the secret door.\n"],
+    "chapter2": ["On the day of the planned escape, a purple curse cloud \n",
+                 "arose… The Queen’s Army has stormed the Castle, \n",
+                 "forcing Snow and Charming to rush to the wardrobe. \n",
+                 "Alas, their baby decided to come.\n\n",
+                 "The army is at the end of the corridor. \n\n",
+                 "Should Snow and Charming:\n\n",
+                 "1. Fight the Queen's army.\n",
+                 "2. Escape through the secret door.\n"],
     "fight_army": ["\nCharming marches towards the army to protect Snow \n",
                    "and their child. Yet, the army is too vast, and all \n",
                    "three of them are captured. The curse comes and \n",
@@ -104,7 +102,7 @@ story_dict = {
 }
 
 ascii_dict = {
-    "title_logo": [r"""         
+    "title_logo": [r"""
   ___                _   _
  / _ \ _ _  __ ___  | | | |_ __  ___ _ _
 | (_) | ' \/ _/ -_) | |_| | '_ \/ _ \ ' \
@@ -112,30 +110,30 @@ ascii_dict = {
   __ _  | |_(_)_ __  ___   |_|
  / _` | |  _| | '  \/ -_)_ _ _
  \__,_|  \__|_|_|_|_\___(_|_|_)
- 
- """],
+
+"""],
     "instructions": [r"""
- _  _              _              _           
-| || |_____ __ __ | |_ ___   _ __| |__ _ _  _ 
+ _  _              _              _
+| || |_____ __ __ | |_ ___   _ __| |__ _ _  _
 | __ / _ \ V  V / |  _/ _ \ | '_ \ / _` | || |
 |_||_\___/\_/\_/   \__\___/ | .__/_\__,_|\_, |
-                             |_|          |__/ 
+                             |_|          |__/
     """],
     "chapter1": [r"""
-  ___ _              _             _ 
+  ___ _              _             _
  / __| |_  __ _ _ __| |_ ___ _ _  / |
 | (__| ' \/ _` | '_ \  _/ -_) '_| | |
  \___|_||_\__,_| .__/\__\___|_|   |_|
-               |_|                   
+               |_|
 
     """],
     "chapter2": [r"""
-    
-  ___ _              _             ___ 
+
+  ___ _              _             ___
  / __| |_  __ _ _ __| |_ ___ _ _  |_  )
-| (__| ' \/ _` | '_ \  _/ -_) '_|  / / 
+| (__| ' \/ _` | '_ \  _/ -_) '_|  / /
  \___|_||_\__,_| .__/\__\___|_|   /___|
-               |_|                     
+               |_|
 
     """]
 }
